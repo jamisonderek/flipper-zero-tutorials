f3b8ae012fe4a61115a6b9b9add119dd618f2d71
Derek Jamison <jamisonderek@users.noreply.github.com>
2024-09-30 19:45:56 -0600

    Update for best practices

--- ui/viewport_demo/demo.c
+++ ui/viewport_demo/demo.c
@@ -14,52 +14,44 @@ also the ViewPort section of the User Interface wiki page
 
 */
 
+#include "dolphin/dolphin.h"
 #include <furi.h>
 #include <gui/gui.h>
 
-typedef enum {
-    MyEventTypeKey,
-    MyEventTypeDone,
-} MyEventType;
-
-typedef struct {
-    MyEventType type; // The reason for this event.
-    InputEvent input; // This data is specific to keypress data.
-} MyEvent;
-
-FuriMessageQueue* queue;
-int x = 32;
-int y = 48;
+typedef struct AppState AppState;
+struct AppState {
+    FuriMutex* mutex;
+    int x;
+    int y;
+};
 
 static void my_draw_callback(Canvas* canvas, void* context) {
-    UNUSED(context);
+    AppState* data = context;
     canvas_set_font(canvas, FontPrimary);
     canvas_draw_str(canvas, 5, 30, "Hello world");
-    canvas_draw_str(canvas, x, y, "^");
+
+    furi_mutex_acquire(data->mutex, FuriWaitForever);
+    canvas_draw_str(canvas, data->x, data->y, "^");
+    furi_mutex_release(data->mutex);
 }
 
 static void my_input_callback(InputEvent* input_event, void* context) {
-    UNUSED(context);
-    if(input_event->type == InputTypeShort) {
-        if(input_event->key == InputKeyLeft) x--;
-        if(input_event->key == InputKeyRight) x++;
-        if(input_event->key == InputKeyUp) y--;
-        if(input_event->key == InputKeyDown) y++;
-        if(input_event->key == InputKeyBack) {
-            MyEvent event;
-            event.type = MyEventTypeDone;
-            furi_message_queue_put(queue, &event, FuriWaitForever);
-        }
-    }
+    FuriMessageQueue *queue = context;
+    furi_message_queue_put(queue, input_event, FuriWaitForever);
 }
 
 int32_t viewport_demo_app() {
-    void* my_context = NULL;
-    queue = furi_message_queue_alloc(8, sizeof(MyEvent));
+    FuriMessageQueue* queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+
+    AppState* app_state = malloc(sizeof(AppState));
+    app_state->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    // Set initial coordinates for the cursor
+    app_state->x = 32;
+    app_state->y = 48;
 
     ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, my_draw_callback, my_context);
-    view_port_input_callback_set(view_port, my_input_callback, my_context);
+    view_port_draw_callback_set(view_port, my_draw_callback, app_state);
+    view_port_input_callback_set(view_port, my_input_callback, queue);
     //view_port_set_orientation(view_port, ViewPortOrientationHorizontal);
     //view_port_set_orientation(view_port, ViewPortOrientationHorizontalFlip); // upside down
     view_port_set_orientation(view_port, ViewPortOrientationVertical); // USB/keypad bottom
@@ -68,22 +60,49 @@ int32_t viewport_demo_app() {
     Gui* gui = furi_record_open(RECORD_GUI);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    MyEvent event;
+    dolphin_deed(DolphinDeedPluginGameStart);
+    
+    InputEvent event;
     bool keep_processing = true;
     while(keep_processing) {
         if(furi_message_queue_get(queue, &event, FuriWaitForever) == FuriStatusOk) {
-            if(event.type == MyEventTypeDone) {
-                keep_processing = false;
+            furi_mutex_acquire(app_state->mutex, FuriWaitForever);
+            if(event.type == InputTypeShort || event.type == InputTypeRepeat) {
+                switch (event.key) {
+                    case InputKeyLeft:
+                        app_state->x--;
+                        break;
+                    case InputKeyRight:
+                        app_state->x++;
+                        break;
+                    case InputKeyUp:
+                        app_state->y--;
+                        break;
+                    case InputKeyDown:
+                        app_state->y++;
+                        break;
+                    case InputKeyBack:
+                        keep_processing = false;
+                        break;
+                    default:
+                        break;
+                }
             }
+            furi_mutex_release(app_state->mutex);
+
+            view_port_update(view_port);
         } else {
             keep_processing = false;
         }
     }
 
-    furi_message_queue_free(queue);
     view_port_enabled_set(view_port, false);
+    furi_message_queue_free(queue);
     gui_remove_view_port(gui, view_port);
-    furi_record_close(RECORD_GUI);
     view_port_free(view_port);
+    furi_record_close(RECORD_GUI);
+    furi_mutex_free(app_state->mutex);
+    free(app_state);
+
     return 0;
 }
---
+++ ui/viewport_demo/demo_minimal.c
@@ -0,0 +1,61 @@
+/*
+
+Please see demo.c for the recommended way to write this app.
+
+This is NOT the recommended way to write an app, but rather an example that would work:
+- It is not recommended to use global variables for the x and y coordinates.
+- The x and y coordinates are not protected by a mutex, so they could be modified by the input callback and the draw callback at the same time.
+- The draw callback should only access state using the context pointer.
+- The input callback should queue the input events, for processing in the main loop.
+
+*/
+#include "dolphin/dolphin.h"
+#include <furi.h>
+#include <gui/gui.h>
+
+static int x = 32;
+static int y = 48;
+
+static void my_draw_callback(Canvas* canvas, void* context) {
+    UNUSED(context);
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str(canvas, 5, 30, "Hello world");
+    canvas_draw_str(canvas, x, y, "^");
+}
+
+static void my_input_callback(InputEvent* event, void* context) {
+    FuriMessageQueue *queue = context;
+    if(event->type == InputTypeShort || event->type == InputTypeRepeat) {
+        if (event->key == InputKeyLeft) x--;
+        if (event->key == InputKeyRight) x++;
+        if (event->key == InputKeyUp) y--;
+        if (event->key == InputKeyDown) y++;
+    }
+    furi_message_queue_put(queue, event, FuriWaitForever);
+}
+
+int32_t viewport_demo_minimal_app() {
+    FuriMessageQueue* queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, my_draw_callback, NULL);
+    view_port_input_callback_set(view_port, my_input_callback, queue);
+    view_port_set_orientation(view_port, ViewPortOrientationVertical); // USB/keypad bottom
+    Gui* gui = furi_record_open(RECORD_GUI);
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+    dolphin_deed(DolphinDeedPluginGameStart);
+    InputEvent event;
+    while(true) {
+        if(furi_message_queue_get(queue, &event, FuriWaitForever) == FuriStatusOk) {
+            if(event.type == InputTypeShort && event.key == InputKeyBack) {
+                break;
+            }
+            view_port_update(view_port);
+        }
+    }
+    view_port_enabled_set(view_port, false);
+    furi_message_queue_free(queue);
+    gui_remove_view_port(gui, view_port);
+    view_port_free(view_port);
+    furi_record_close(RECORD_GUI);
+    return 0;
+}
